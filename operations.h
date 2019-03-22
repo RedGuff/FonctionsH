d2b835a127523db048d8937c738fb9d7a6fb1586
Saussereau Dominique <domsau2@yahoo.fr>
2019-03-22 15:40:52 +0100

    Simplification.

--- operations.h
+++ operations.h
@@ -1,12 +1,7 @@
 #ifndef OPERATIONS_H_INCLUDED
 #define OPERATIONS_H_INCLUDED
 
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int add(int a_local_add, int b_local_add);
 int diff(int a_local_diff, int b_local_diff);
-
-#endif // HEADER_FILE
 
 #endif // OPERATIONS_H_INCLUDED
