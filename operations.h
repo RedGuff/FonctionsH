d7369c3de7998fb65f76f678d1517b24364e0440
Saussereau Dominique <domsau2@yahoo.fr>
2019-05-10 21:09:35 +0200

    defaults = 0

--- operations.h
+++ operations.h
@@ -1,7 +1,7 @@
 #ifndef OPERATIONS_H_INCLUDED
 #define OPERATIONS_H_INCLUDED
 
-int add(int a_local_add, int b_local_add);
-int diff(int a_local_diff, int b_local_diff);
+int add(int a_local_add = 0, int b_local_add = 0);
+int diff(int a_local_diff = 0, int b_local_diff = 0);
 
 #endif // OPERATIONS_H_INCLUDED
